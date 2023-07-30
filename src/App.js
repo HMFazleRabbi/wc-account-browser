8bc7b8440e0509fc3588cc1335ef7b157abc92de
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-31 03:00:29 +0600

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+require('dotenv').config();
+import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
 
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+
+// Configure chains & providers with the Infura (infura.io) provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [infuraProvider({ apiKey: process.env.INFURA_PROJECT_ID }), publicProvider()],
+)
+
+// Set up wagmi config
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: process.env.WALLET_CONNECT_PROJECT_ID,
+      },
+    }),
+    // new InjectedConnector({
+    //   chains,
+    //   options: {
+    //     name: 'Injected',
+    //     shimDisconnect: true,
+    //   },
+    // }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
+
+// Pass config to React Context Provider
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <WagmiConfig config={config}>
+      <Profile />
+    </WagmiConfig>
+  )
 }
 
 export default App;
