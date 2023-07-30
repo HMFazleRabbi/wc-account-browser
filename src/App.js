ee90f68198a50656ff83c978b69c7c82c73f1350
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-31 03:10:26 +0600

    Working connect button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
+import './App.css';
 
+import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
+ 
 import { infuraProvider } from 'wagmi/providers/infura'
 import { publicProvider } from 'wagmi/providers/public'
-
+ 
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
+import Profile from './Profile';
 
-// Configure chains & providers with the Infura (infura.io) provider.
+// Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
-  [infuraProvider({ apiKey: process.env.INFURA_PROJECT_ID }), publicProvider()],
+  [infuraProvider({ apiKey: 'ae891e481aba4cb492604b551481c8cb' }), publicProvider()],
 )
-
 // Set up wagmi config
 const config = createConfig({
   autoConnect: true,
@@ -31,7 +31,7 @@ const config = createConfig({
     new WalletConnectConnector({
       chains,
       options: {
-        projectId: process.env.WALLET_CONNECT_PROJECT_ID,
+        projectId: '798e6251cecb5d1c1d3ee54d1f71e24b',
       },
     }),
     // new InjectedConnector({
@@ -46,13 +46,12 @@ const config = createConfig({
   webSocketPublicClient,
 })
 
-// Pass config to React Context Provider
 function App() {
   return (
     <WagmiConfig config={config}>
       <Profile />
     </WagmiConfig>
-  )
+  );
 }
 
 export default App;
